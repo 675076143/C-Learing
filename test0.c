603eba38c1d69fd608a99f2de9067f0c736b5583
RobinJohn <Robin675076143@hotmail.com>
2019-02-27 23:10:31 +0800

    20190227

---
+++ test0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int a [] = {1,2,3,4,5,6,7,8,9,0};
+    char b [] = {"1231235"};
+    printf("The size of a[] is %d\n",sizeof(a));
+    printf("The length of a[] is %d\n",sizeof(a)/sizeof(a[0]));
+    printf("The strlen of b [] is %d\n",strlen(b));
+    printf("githubTest100100");
+    printf("This is a test");
+}
